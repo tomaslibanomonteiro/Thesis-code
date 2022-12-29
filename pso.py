1eb297bf7657b62ab86e76333679e7f1f1782387
tomaslibanomonteiro <83981899+tomaslibanomonteiro@users.noreply.github.com>
2022-12-29 03:58:37 +0000

    pso plot with different seeds

--- pso.py
+++ pso.py
@@ -13,18 +13,20 @@ from pymoo.core.callback import Callback
 from pymoo.algorithms.soo.nonconvex.pso import PSO
 
 # DEFINES
-N_EVALS = 1000
+N_EVALS = 200
 DEBUG_PRINT = True
 VERBOSE = False
 SAVE_HISTORY = False
 PROB_NAMES = ['Rastrigin']
 
 # PSO
-POP_SIZE_LIST = [5, 25, 50, 100]
+POP_SIZE_LIST = [5, 10, 20]
 W_LIST = [0.7]
 C1_LIST = [0.7]
 C2_LIST = [0.7]
 
+# number of times the algorithm is run for each combination of parameters
+N_SEEDS = 20
 
 JUST_PLOT = False
 
@@ -33,14 +35,14 @@ def debug_print(msg, debug_print):
     if debug_print:
         print(msg)
 
-def print_pso(algo, debug_print):
+def print_pso(algo, algo_id, seed, debug_print):
     if not debug_print:
         return
 
-    print(" algo name: ", algo.__class__.__name__)
+    print(" algo name: ", algo.__class__.__name__, algo_id, "seed: ", seed)
     print("     Parameterers:  w=", algo.w, "  c1=", algo.c1, "  c2=", algo.c2, "  pop_size=", algo.pop_size)
 
-################################### DATA HANDLING ######################################
+################################### PLOTTING ######################################
 
 def legendPlot(x, pm_name, prob, figname=None):
     plt.legend()
@@ -65,38 +67,46 @@ def plotPM(data, each_plot = 'prob', plotting_mode = 'by_evals', save=True, pm_n
     else:
         raise ValueError("plotting_mode must be 'by_evals' or 'by_time'")
 
-    if each_plot == 'prob':
-        
+    # average over runs with different seeds for each problem and algorithm
+    data = data.groupby(['prob', 'algo', 'n_evals']).mean().reset_index()
+
+    if each_plot == 'prob':        
         for prob in data['prob'].unique():
             for algo in data['algo'].unique():
-                df = data[(data['prob'] == prob) & (data['algo'] == algo)] 
-                plt.plot(df[x], df[pm_name], label=algo)
+                df = data[(data['prob'] == prob) & (data['algo'] == algo)]
+                # set label with algo name and parameters with 2 decimal places each
+                
+                label = algo.split('_')[0] + ' w=' + str(round(df['w'].iloc[0], 2)) + ' c1=' + str(round(df['c1'].iloc[0], 2)) + ' c2=' + str(round(df['c2'].iloc[0], 2)) + ' pop_size=' + str(df['pop_size'].iloc[0])
+                plt.plot(df[x], df[pm_name], label=label)
             legendPlot(x, pm_name, prob, 'figures/prob_' + prob + '-pm_' + pm_name +'-x=' + x + '.png')
 
     elif each_plot == 'algo':
         for algo in data['algo'].unique():
             for prob in data['prob'].unique():
-                df = data[(data['prob'] == prob) & (data['algo'] == algo)] 
-                plt.plot(df[x], df[pm_name], label=prob)
+                label = algo.split('_')[0] + ' w=' + str(round(df['w'].iloc[0], 2)) + ' c1=' + str(round(df['c1'].iloc[0], 2)) + ' c2=' + str(round(df['c2'].iloc[0], 2)) + ' pop_size=' + str(df['pop_size'].iloc[0])
+                plt.plot(df[x], df[pm_name], label=label)
             legendPlot(x, pm_name, algo, 'figures/algo_' + algo + '-pm_' + pm_name +'-x=' + x + '.png')
 
     elif each_plot == 'prob_algo':
         for prob in data['prob'].unique():
             for algo in data['algo'].unique():
                 df = data[(data['prob'] == prob) & (data['algo'] == algo)] 
-                plt.plot(df[x], df[pm_name], label=algo)    
+                label = algo.split('_')[0] + ' w=' + str(round(df['w'].iloc[0], 2)) + ' c1=' + str(round(df['c1'].iloc[0], 2)) + ' c2=' + str(round(df['c2'].iloc[0], 2)) + ' pop_size=' + str(df['pop_size'].iloc[0])
+                plt.plot(df[x], df[pm_name], label=label)
+                legendPlot(x, pm_name, prob, 'figures/prob_' + prob + '-pm_' + pm_name +'-x=' + x + '.png')
+
     else:
         raise ValueError("each_plot must be 'by_problem', 'by_algo' or 'all_plots'")
 
-def update_data(data, res, algo_id):
+################################### DATA HANDLING ######################################
+
+def update_data(data, res, algo_id, run_id, seed):
 
     # get data from callback
     n_evals = res.algorithm.callback.n_evals
-    best_list = res.algorithm.callback.best
+    pm = res.algorithm.callback.best
     time = res.algorithm.callback.time
     time = [t - time[0] for t in time]
-
-    pm = [best[0] for best in best_list]
 
     # get data in 
     algo = [res.algorithm.__class__.__name__ + '_' + str(algo_id)] * len(pm)
@@ -105,12 +115,14 @@ def update_data(data, res, algo_id):
     c1 = [res.algorithm.c1] * len(pm)
     c2 = [res.algorithm.c2] * len(pm)
     prob = [res.problem.__class__.__name__] * len(pm)
-    
+    run_id = [run_id] * len(pm)
+    seed = [seed] * len(pm)
+
     # create data frame 
-    prob_algo_data = pd.DataFrame({'prob': prob, 'algo': algo, 'pop_size': pop_size, 
+    run_data = pd.DataFrame({'run_id': run_id, 'prob': prob, 'algo': algo, 'seed': seed, 'pop_size': pop_size, 
                                     'w': w, 'c1': c1, 'c2': c2, 'best': pm, 'n_evals': n_evals, 'time': time})
     
-    return pd.concat([data, prob_algo_data])
+    return pd.concat([data, run_data])
 
 ################################### PROBLEM ######################################
 
@@ -132,7 +144,7 @@ class MyCallback(Callback):
 
     def notify(self, algo):
         feas = np.where(algo.opt.get("feasible"))[0]
-        self.best.append(algo.pop.get("F")[feas])
+        self.best.append(algo.pop.get("F")[feas][0][0])
         self.n_evals.append(algo.evaluator.n_eval)
         self.time.append(time.perf_counter())
 
@@ -149,26 +161,31 @@ def set_pso_list():
 
 def main():
     data = pd.DataFrame()   
+    run_id = 0
     for prob in set_prob_list(PROB_NAMES):
         debug_print("prob: " + prob.__class__.__name__, DEBUG_PRINT)
         for algo_id, algo in enumerate(set_pso_list()):  
-            print_pso(algo, DEBUG_PRINT)
-            
-            res = minimize(prob,
-                        algo,
-                        ('n_evals', N_EVALS),
-                        seed=1,
-                        verbose=VERBOSE,
-                        save_history=SAVE_HISTORY,
-                        callback=MyCallback())
+            for seed in range(N_SEEDS):
+                    
+                print_pso(algo, algo_id, seed, DEBUG_PRINT)
+                
+                res = minimize(prob,
+                            algo,
+                            ('n_evals', N_EVALS),
+                            seed=seed,
+                            verbose=VERBOSE,
+                            save_history=SAVE_HISTORY,
+                            callback=MyCallback())
 
-            data = update_data(data, res, algo_id)
+                data = update_data(data, res, algo_id, run_id, seed)
+                run_id += 1
 
     # save data
     data.to_csv('data.csv', index=False)
 
     # plot data
     plotPM(data, each_plot = 'prob', plotting_mode = 'by_evals', save=True)
+    plotPM(data, each_plot = 'prob', plotting_mode = 'by_time', save=True)
 
 def just_plot():
     data = pd.read_csv('data.csv')
