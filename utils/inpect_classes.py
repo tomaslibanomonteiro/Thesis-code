97dab64c0851d4405c98d9b3a21e816920f20fc5
tomaslibanomonteiro <83981899+tomaslibanomonteiro@users.noreply.github.com>
2023-04-21 04:05:33 +0100

    inspect classes better

--- utils/inpect_classes.py
+++ utils/inpect_classes.py
@@ -16,12 +16,12 @@ class Defaults():
         self.prob = self.get_class_list(get_problem_options())
         self.term = self.get_class_list(get_termination_options())
         self.pi = self.get_class_list(get_performance_indicator_options())
-        self.algo = self.get_class_list(get_algorithm_options())
+        self.algo = self.get_class_list(get_algorithm_options(), algo_inspection=True)
 
-    def get_class_list(self, options_list):
-        return [self.classInpection(name, obj) for name, obj in options_list]
+    def get_class_list(self, options_list, algo_inspection = False):
+        return [self.classInpection(name, obj, algo_inspection=algo_inspection) for name, obj in options_list]
     
-    def nestedClass(self, prev_object_id, args, i, options_list):
+    def nestedClass(self, prev_object_id: str, args: list, i: int, options_list: list, list_to_append: list):
         
         arg, value = args[i]
         
@@ -31,19 +31,37 @@ class Defaults():
         for class_name, obj in options_list:
             if isinstance(obj, value.__class__):
                 break
-        self.mutation.append(self.classInpection(class_name, value, nested_object_id))
+        list_to_append.append(self.classInpection(class_name, value, nested_object_id))
             
-    def classInpection(self, class_name: str, cls: type, object_id = "default"):
+    def classInpection(self, class_name: str, cls: type, object_id = "default", algo_inspection = False):
         
         if object_id == "default": object_id = "default_" + class_name
         sig = inspect.signature(cls.__init__)
         
-        args = [(arg, sig.parameters[arg].default) for arg in sig.parameters.keys() if arg not in ["self", "args", "kwargs"]]
-
-        # deal with nested classes
-        for i, (arg, value) in enumerate(args):
-            if arg == "mutation":
-                self.nestedClass(object_id, args, i, get_mutation_options())
-                
+        args = []
+        for arg in sig.parameters.keys():
+            if arg in ["self", "args", "kwargs"]: continue
+            if sig.parameters[arg].default is inspect.Parameter.empty:
+                value = "no default (need to change)"
+            else:
+                value = sig.parameters[arg].default
+            args.append((arg, value))
         
-        return [(object_id, class_name)] + args 
+        if algo_inspection:
+            # deal with nested classes
+            for i, (arg, value) in enumerate(args):
+                if arg == "mutation":   
+                    self.nestedClass(object_id, args, i, get_mutation_options(), self.mutation)
+                elif arg == "crossover":
+                    self.nestedClass(object_id, args, i, get_crossover_options(), self.crossover)
+                elif arg == "selection":
+                    self.nestedClass(object_id, args, i, get_selection_options(), self.selection)
+                elif arg == "decomposition":
+                    self.nestedClass(object_id, args, i, get_decomposition_options(), self.decomposition)
+                elif arg == "sampling":
+                    self.nestedClass(object_id, args, i, get_sampling_options(), self.sampling)
+                elif arg == "ref_dirs":
+                    self.nestedClass(object_id, args, i, get_reference_direction_options(), self.ref_dirs)
+            
+        final_list = [object_id, class_name] + [item for tpl in args for item in tpl]                
+        return final_list
