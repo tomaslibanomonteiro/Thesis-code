e1b4972b6a4d775944e5a2d098d1b9217bd375b6
tomaslibanomonteiro <83981899+tomaslibanomonteiro@users.noreply.github.com>
2024-02-09 05:32:40 +0000

    tests_working again 2

--- run_tests.py
+++ run_tests.py
@@ -1,13 +1,15 @@
 import os
 import filecmp
+import time
 
 # import all tests and declare them in ALL_TESTS
 from utils.defines import RESULTS_FOLDER, EXPECTED_RESULTS_FOLDER, RESULTS_FILE
 from PyQt5.QtWidgets import QApplication
 
-from tests.tests_declaration import soo_algos, soo_probs, soo_mixed, moo_algos, moo_probs, moo_mixed
+from tests.tests_declaration import Test, soo_algos, soo_probs, soo_mixed, moo_algos, moo_probs, moo_mixed
 
-TESTS_TO_RUN = [moo_probs]
+# TESTS_TO_RUN = [moo_probs, moo_algos, moo_mixed, soo_probs, soo_algos, soo_mixed]
+TESTS_TO_RUN = [soo_probs, soo_algos, soo_mixed]
 
 def main():
 
@@ -16,37 +18,39 @@ def main():
         import shutil
         shutil.rmtree(RESULTS_FOLDER)    
     os.mkdir(RESULTS_FOLDER)
-        
-    # create a QApplication to instatiate the QWidgets
-    app = QApplication([]) 
     
+    # create the app to instantiate the MainWindows
+    app = QApplication([])
+    tests=[]    
     # run the tests
-    for test in TESTS_TO_RUN: 
-        print('Running test ' + test.test_name + '...')
-        test.runTest()
-
-    # wait for all threads to finish
-    for my_thread in [test.my_thread for test in TESTS_TO_RUN]:
-        print('Waiting for thread ' + str(my_thread) + ' to finish...')
-        my_thread.join() 
+    for test_options in TESTS_TO_RUN: 
+        test = Test(test_options)
+        tests.append(test)
+        test.run()
     
+    # Wait for all tests to finish
+    for test in tests:
+        while not test.is_finished:
+            app.processEvents()
+            time.sleep(0.1)
+            
     # get the list of files to compare
     expected_files = [file for file in os.listdir(EXPECTED_RESULTS_FOLDER)]
     files = [file for file in os.listdir(RESULTS_FOLDER)]
             
     # compare the CSV files
     for file in files:
-        # write the results in a file inside RESULTS_FOLDER
-        with open(RESULTS_FILE, 'w') as f:
+        # append the results to the file inside RESULTS_FOLDER
+        with open(RESULTS_FILE, 'a') as f:
             if file not in expected_files:
-                f.write('No comparison for test ' + file)
+                f.write('No comparison for test ' + file + '\n')
             else:
                 import datetime
                 date_time = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
                 if filecmp.cmp(RESULTS_FOLDER + '/' + file, EXPECTED_RESULTS_FOLDER + '/' + file):
-                    f.write(date_time + ' Test ' + file + ' passed!')
+                    f.write(date_time + ' Test ' + file + ' passed!\n')
                 else:
-                    f.write(date_time + ' Test ' + file + ' failed!')
+                    f.write(date_time + ' Test ' + file + ' failed!\n')
 
 if __name__ == '__main__':
     main()
--- tests/tests_declaration.py
+++ tests/tests_declaration.py
@@ -1,38 +1,50 @@
 from backend.defaults import Defaults
 from frontend.main_window import MyMainWindow
-from threading import Thread
-from utils.defines import RESULTS_FOLDER, RUN_OPTIONS_KEYS, PROB_KEY, ALGO_KEY, PI_KEY, TERM_KEY, N_SEEDS_KEY
+from utils.defines import RESULTS_FOLDER, RUN_OPTIONS_KEYS, PROB_KEY, ALGO_KEY, PI_KEY, TERM_KEY, N_SEEDS_KEY, MOO_KEY
 
+TEST_NAME_KEY = 'test_name'
 class Test():
-    def __init__(self, test_name: str, options: dict, defaults_soo = Defaults('soo').dict, defaults_moo = Defaults('moo').dict) -> None:
+    def __init__(self, options: dict, n_seeds:int=1, n_evals:int=500, parameters=None):
         
-        self.test_name = test_name + '.csv'
+        self.test_name = options.pop(TEST_NAME_KEY) + '.csv'
+        self.is_finished = False
         
-        if set(options.keys()) != set(RUN_OPTIONS_KEYS + ['moo']):
-            raise Exception('Invalid options dictionary! Test options must contain: ' + str(RUN_OPTIONS_KEYS + ['moo']))
+        if N_SEEDS_KEY not in options.keys():
+            options[N_SEEDS_KEY] = n_seeds
+        if TERM_KEY not in options.keys():
+            options[TERM_KEY] = ['n_eval']
+        if 'n_evals' in options.keys():
+            n_evals = options.pop('n_evals')
+
+        if set(options.keys()) != set(RUN_OPTIONS_KEYS + [MOO_KEY]):
+            raise Exception('Invalid options dictionary. Test options must contain at least: ' + str([MOO_KEY, PI_KEY, ALGO_KEY, PROB_KEY]))
         self.options = options
-        self.moo = self.options.pop('moo') 
-        self.defaults_soo = defaults_soo
-        self.defaults_moo = defaults_moo
+        self.moo = self.options.pop(MOO_KEY) 
         
-        self.main_window = None
-        self.run = None
-        self.my_thread = Thread(target=self.runTestThread)
-      
-    def runTest(self):
+        if parameters is None:
+            self.parameters = Defaults(self.moo).dict
+        else:
+            self.parameters = parameters
+        self.parameters[TERM_KEY]['n_eval']['n_max_evals'] = n_evals
+        
         # get main window, and run object 
-        run_options_soo = self.options if not self.moo else {}
-        run_options_moo = self.options if self.moo else {}
+        run_options_soo, parameters_soo = (self.options, self.parameters) if not self.moo else ({}, Defaults(False).dict)
+        run_options_moo, parameters_moo = (self.options, self.parameters) if self.moo else ({}, Defaults(True).dict)
         
-        self.main_window = MyMainWindow(run_options_soo, run_options_moo, self.defaults_soo, self.defaults_moo)
-        self.run = self.main_window.activePage().runButton()
-        
-        # start a python thread 
-        self.my_thread.start()
+        self.main_window = MyMainWindow(run_options_soo, run_options_moo, parameters_soo, parameters_moo)
+        self.run_thread = self.main_window.activeTabs().getRunThread()
+        self.run_thread.finished.connect(self.afterRun)
     
-    def runTestThread(self):
-        self.run.run()
-        self.run.save_data(RESULTS_FOLDER + '/' + self.test_name)
+    def run(self):
+        self.run_thread.start()
+        
+    def afterRun(self):
+        import debugpy
+        debugpy.debug_this_thread() #!
+        
+        data = self.run_thread.data
+        data.to_csv(RESULTS_FOLDER + '/' + self.test_name, index=False)
+        self.is_finished = True
 
 ############################################################################################################
 ######################################### SOO TESTS ########################################################
@@ -41,90 +53,55 @@ class Test():
 # 'brkga' is elementwise algo?
 # 'cmaes' and 'pso' not giving consistent results
 
-run_options_soo_algos = {
-    'moo': False,
-    N_SEEDS_KEY: 2,
-    TERM_KEY: ['n_eval'],
+soo_algos = { 
+    MOO_KEY: False,
     PI_KEY: ['best'],
-    ALGO_KEY: ['ga', 'de', 'nelder-mead', 'pattern-search'],
-    PROB_KEY: ['ackley']
+    ALGO_KEY: ['ga', 'de', 'nelder-mead', 'pattern-search', 'cmaes', 'pso'],
+    PROB_KEY: ['ackley'],
+    TEST_NAME_KEY: 'soo_algos',
 }
 
-soo_algos = Test('soo_algos', run_options_soo_algos)    
-
-run_options_test_soo_probs = {
-    'moo': False,
-    N_SEEDS_KEY: 2,
-    TERM_KEY: ['n_eval'],
+# 'knp' not set?
+soo_probs = {
+    MOO_KEY: False,
     PI_KEY: ['best'],
     ALGO_KEY: ['de'],
-    PROB_KEY: ['ackley', 'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'g9', 'g10', 'g11', 'g12', 'g13', 'g14', 'g15', 'g16', 'g17', 'g18', 'g19', 'g20', 'g21', 'g22', 'g23', 'g24', 'cantilevered_beam', 'griewank', 'himmelblau', 'knp', 'pressure_vessel', 'rastrigin', 'rosenbrock', 'schwefel', 'sphere', 'zakharov']
+    PROB_KEY: ['ackley', 'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'g9', 'g10', 'g11', 'g12', 'g13', 'g14', 'g15', 'g16', 'g17', 'g18', 'g19', 'g20', 'g21', 'g22', 'g23', 'g24', 'cantilevered_beam', 'griewank', 'himmelblau', 'pressure_vessel', 'rastrigin', 'rosenbrock', 'schwefel', 'sphere', 'zakharov'],
+    TEST_NAME_KEY: 'soo_probs'
 }
 
-soo_probs = Test('soo_probs', run_options_test_soo_probs)
-
-run_options_soo_mixed = {
-    'moo': False,
-    N_SEEDS_KEY: 2,
-    TERM_KEY: ['n_eval'],
+soo_mixed = {
+    MOO_KEY: False,
     PI_KEY: ['best'],
     ALGO_KEY: ['ga', 'pso', 'de'],
-    PROB_KEY: ['ackley','g1','griewank','rastrigin','rosenbrock']
-}        
-
-soo_mixed = Test('soo_mixed', run_options_soo_mixed)
-
-run_options_soo_simple = {
-    'moo': False,
-    N_SEEDS_KEY: 2,
-    TERM_KEY: ['n_eval'],
-    PI_KEY: ['best'],
-    ALGO_KEY: ['ga'],
-    PROB_KEY: ['ackley']
+    PROB_KEY: ['ackley','g1','griewank','rastrigin','rosenbrock'],
+    TEST_NAME_KEY: 'soo_mixed'
 }
     
 ############################################################################################################
 ######################################### MOO TESTS ########################################################
 ############################################################################################################
 
-run_options_moo_algos = {
-    'moo': True,
-    N_SEEDS_KEY: 2,
-    TERM_KEY: ['n_eval'],
+moo_algos = {
+    MOO_KEY: True,
     ALGO_KEY: ['nsga2', 'rnsga2', 'nsga3', 'unsga3', 'rnsga3', 'moead', 'ctaea'],
     PI_KEY: ['gd+'],
-    PROB_KEY: ['dtlz4']
+    PROB_KEY: ['dtlz4'],
+    TEST_NAME_KEY: 'moo_algos'
 }
 
-moo_algos = Test('moo_algos', run_options_moo_algos)
-
-run_options_moo_probs = {
-    'moo': True,
-    N_SEEDS_KEY: 1,
-    TERM_KEY: ['n_eval'],
+moo_probs = {
+    MOO_KEY: True,
     PI_KEY: ['gd+'],
     ALGO_KEY: ['nsga2'],
-    PROB_KEY: ['bnh', 'carside', 'ctp1', 'ctp2', 'ctp3', 'ctp4', 'ctp5', 'ctp6', 'ctp7', 'ctp8', 'dascmop1', 'dascmop2', 'dascmop3', 'dascmop4', 'dascmop5', 'dascmop6', 'dascmop7', 'dascmop8', 'dascmop9', 'df1', 'df2', 'df3', 'df4', 'df5', 'df6', 'df7', 'df8', 'df9', 'df10', 'df11', 'df12', 'df13', 'df14', 'modact', 'mw1', 'mw2', 'mw3', 'mw4', 'mw5', 'mw6', 'mw7', 'mw8', 'mw9', 'mw10', 'mw11', 'mw12', 'mw13', 'mw14', 'dtlz1^-1', 'dtlz1', 'dtlz2', 'dtlz3', 'dtlz4', 'dtlz5', 'dtlz6', 'dtlz7', 'convex_dtlz2', 'convex_dtlz4', 'sdtlz1', 'c1dtlz1', 'c1dtlz3', 'c2dtlz2', 'c3dtlz1', 'c3dtlz4', 'dc1dtlz1', 'dc1dtlz3', 'dc2dtlz1', 'dc2dtlz3', 'dc3dtlz1', 'dc3dtlz3', 'kursawe', 'osy', 'srn', 'tnk', 'truss2d', 'welded_beam', 'zdt1', 'zdt2', 'zdt3', 'zdt4', 'zdt5', 'zdt6', 'wfg1', 'wfg2', 'wfg3', 'wfg4', 'wfg5', 'wfg6', 'wfg7', 'wfg8', 'wfg9']
+    PROB_KEY: ['bnh', 'carside', 'ctp1', 'ctp2', 'ctp3', 'ctp4', 'ctp5', 'ctp6', 'ctp7', 'ctp8', 'dascmop1', 'dascmop2', 'dascmop3', 'dascmop4', 'dascmop5', 'dascmop6', 'dascmop7', 'dascmop8', 'dascmop9', 'df1', 'df2', 'df3', 'df4', 'df5', 'df6', 'df7', 'df8', 'df9', 'df10', 'df11', 'df12', 'df13', 'df14', 'modact', 'mw1', 'mw2', 'mw3', 'mw4', 'mw5', 'mw6', 'mw7', 'mw8', 'mw9', 'mw10', 'mw11', 'mw12', 'mw13', 'mw14', 'dtlz1^-1', 'dtlz1', 'dtlz2', 'dtlz3', 'dtlz4', 'dtlz5', 'dtlz6', 'dtlz7', 'convex_dtlz2', 'convex_dtlz4', 'sdtlz1', 'c1dtlz1', 'c1dtlz3', 'c2dtlz2', 'c3dtlz1', 'c3dtlz4', 'dc1dtlz1', 'dc1dtlz3', 'dc2dtlz1', 'dc2dtlz3', 'dc3dtlz1', 'dc3dtlz3', 'kursawe', 'osy', 'srn', 'tnk', 'truss2d', 'welded_beam', 'zdt1', 'zdt2', 'zdt3', 'zdt4', 'zdt5', 'zdt6', 'wfg1', 'wfg2', 'wfg3', 'wfg4', 'wfg5', 'wfg6', 'wfg7', 'wfg8', 'wfg9'],
+    TEST_NAME_KEY: 'moo_probs'
 }
 
-moo_probs = Test('moo_prob', run_options_moo_probs)
-
-run_options_moo_mixed = {
-    'moo': True,
-    N_SEEDS_KEY: 2,
-    TERM_KEY: ['n_eval'],
+moo_mixed = {
+    MOO_KEY: True,
     PI_KEY: ['gd', 'gd+', 'igd+', 'igd'],
     ALGO_KEY: ['nsga2', 'nsga3'],
-    PROB_KEY: ['bnh','ctp1']
-}
-
-moo_mixed = Test('moo_mixed_1', run_options_moo_mixed)
-
-run_options_moo_simple = {
-    'moo': True,
-    N_SEEDS_KEY: 2,
-    TERM_KEY: ['n_eval'],
-    PI_KEY: ['gd'],
-    ALGO_KEY: ['nsga2'],
-    PROB_KEY: ['bnh']    
+    PROB_KEY: ['bnh','ctp1'],
+    TEST_NAME_KEY: 'moo_mixed'
 }
