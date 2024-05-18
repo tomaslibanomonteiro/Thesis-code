af484c3bbd3c622635b64abae1a06605eaec1350
tomaslibanomonteiro <83981899+tomaslibanomonteiro@users.noreply.github.com>
2024-05-19 00:42:41 +0100

    dictionaries updated

--- thesis/others/change_parameters.py
+++ thesis/others/change_parameters.py
@@ -2,7 +2,7 @@ import pickle
 import os
 import pickle
 from backend.defaults import Defaults
-
+from utils.defines import MOO_KEY, TERM_KEY, PI_KEY, ALGO_KEY, REF_DIR_KEY, PROB_KEY
 # Function to update the pickle file
 def update_pickle_file(file_path):
     with open(file_path, 'rb') as file:
@@ -12,7 +12,9 @@ def update_pickle_file(file_path):
     user_input = input(f"Do you want to update the file '{file_path}'? (y/n): ")
     
     if user_input.lower() == 'y':
-        data['parameters']['plot_types'] = Defaults(moo=data['moo']).parameters['plot_types']
+        def_parameters = Defaults(moo=data['moo']).parameters
+        data['parameters'][TERM_KEY] = def_parameters[TERM_KEY]
+        data['parameters'][PI_KEY] = def_parameters[PI_KEY]
     
     with open(file_path, 'wb') as file:
         pickle.dump(data, file)
